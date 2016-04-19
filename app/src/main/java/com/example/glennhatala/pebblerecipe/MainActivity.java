6fc8398d2c9f765f5117dc22ecb4c517647443fd
Emily <rowdowser@gmail.com>
2016-04-18 20:32:52 -0400

    can send different types of data to watch

--- app/src/main/java/com/example/glennhatala/pebblerecipe/MainActivity.java
+++ app/src/main/java/com/example/glennhatala/pebblerecipe/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.getpebble.android.kit.PebbleKit;
@@ -17,14 +19,23 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView txt1;
+    Button btn1;
+    Button btn2;
+    Button btn3;
+
+    private static final UUID APP_UUID = UUID.fromString("672ceda8-1ca2-402a-95dd-5109d97bef36");
+    private PebbleKit.PebbleDataReceiver mDataReceiver;
 
     private static final int KEY_BUTTON_UP = 0;
     private static final int KEY_BUTTON_DOWN = 1;
-    private static final UUID APP_UUID = UUID.fromString("672ceda8-1ca2-402a-95dd-5109d97bef36");
-    private PebbleKit.PebbleDataReceiver mDataReceiver;
+
+    private static final int KEY_RESULT = 1;
+    private static final int TYPE_TITLE = 0;
+    private static final int TYPE_INGREDIENT = 1;
+    private static final int TYPE_STEP = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +43,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txt1 = (TextView)findViewById(R.id.txt1);
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        btn3 = (Button)findViewById(R.id.btn3);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        PebbleDictionary dict = new PebbleDictionary();
+        switch(v.getId()) {
+            case R.id.btn1:
+                dict.addInt32(KEY_RESULT, TYPE_TITLE);
+                PebbleKit.sendDataToPebble(getApplicationContext(), APP_UUID, dict);
+                break;
+            case R.id.btn2:
+                dict.addInt32(KEY_RESULT, TYPE_INGREDIENT);
+                PebbleKit.sendDataToPebble(getApplicationContext(), APP_UUID, dict);
+                break;
+            case R.id.btn3:
+                dict.addInt32(KEY_RESULT, TYPE_STEP);
+                PebbleKit.sendDataToPebble(getApplicationContext(), APP_UUID, dict);
+                break;
+        }
     }
 
     @Override
